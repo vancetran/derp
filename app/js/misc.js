d5f92d4b4c6df8d0eec15551dc746628eacd1c46
Vance Tran <v@vancetran.com>
2014-06-26 16:20:31 -0700

    Masonry call refinement

--- app/js/misc.js
+++ app/js/misc.js
@@ -3,16 +3,17 @@ $( document ).ready(function() {
   // Item details toggle
   $(".item-detail-button").click(function(){
       $(this).next(".item-detail").toggle();
+      $msnryContainer.masonry(); // trigger each time to fix the layout
     }
   );
 
   // Masonry
-  imagesLoaded( '#list-view', function() {
-    $('#list-view').masonry()
-      .append( elem )
-      .masonry( 'appended', elem );
-      // no method is same as layout -> .masonry('layout')
-      masonry();
+  var $msnryContainer = $('#list-view');
+
+  imagesLoaded( $msnryContainer, function() {
+    $msnryContainer.masonry();
   });
+
+
 
 });
